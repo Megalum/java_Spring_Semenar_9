b540b244cb5a53fe0777146f434f710146224c8f
stank <stanko.demon@gmail.com>
2024-04-19 17:52:41 +0300

    Semenar

---
+++ issue-service/src/main/java/ru/gb/issue_service/BookProvider.java
@@ -0,0 +1,42 @@
+package ru.gb.issue_service;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+@Component
+public class BookProvider {
+
+    private final WebClient webClient;
+    private final EurekaClient eurekaClient;
+
+    public BookProvider(EurekaClient eurekaClient) {
+        webClient = WebClient.builder().build();
+        this.eurekaClient = eurekaClient;
+    }
+
+    public UUID getRandomBookId() {
+        Book randomBook = webClient.get()
+                .uri(getBookServiceIp() + "/book/random")
+                .retrieve()
+                .bodyToMono(Book.class)
+                .block();
+
+        return randomBook.getId();
+    }
+
+    private String getBookServiceIp(){
+        Application application = eurekaClient.getApplication("BOOK-SERVICE");
+        List<InstanceInfo> instanceInfos = application.getInstances();
+
+        Random random = new Random();
+        InstanceInfo randomInstance = instanceInfos.get(random.nextInt(instanceInfos.size()));
+        return "http://" + randomInstance.getIPAddr() + ":" + randomInstance.getPort();
+    }
+}
